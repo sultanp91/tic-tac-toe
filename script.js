8d7d4bd947bf502fdc40843a695fd29501a81d3a
Sultan <sultanp@gmail.com>
2021-01-06 14:29:47 +0000

    updated game logic to alternate between X and O's

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gameBoardContainer = document.querySelector(".game-board-container");
 
-let boardArray = ["x", "o", "x", "o", "x", "o", "x", "o", "x"]
+let boardArray = ["", "", "", "", "", "", "", "", ""]
 
 let newGameboard = function () {
+    gameBoardContainer.textContent = "";
     for(let i = 0; i < 9; i++){
        let gridBox = document.createElement("div");
        gridBox.textContent = boardArray[i];
@@ -15,3 +16,40 @@ let newGameboard = function () {
 }
 
 newGameboard();
+
+let playerOne = {
+    choice: "x",
+    turn: true,
+}
+
+let playerTwo = {
+    choice: "o",
+    turn: false,
+}
+
+let gameController = {
+    playRound: function () {
+
+    }
+}
+
+gameBoardContainer.addEventListener("click", (e) => {
+    
+    let arrayIndex = e.target.id;
+
+    if(boardArray[arrayIndex] !== "o" && boardArray[arrayIndex] !== "x"){
+        if(playerOne.turn === true){
+            boardArray[arrayIndex] = playerOne.choice;
+            playerOne.turn = false;
+            playerTwo.turn = true;
+            newGameboard();
+        } else if(playerTwo.turn === true){
+            boardArray[arrayIndex] = playerTwo.choice;
+            playerOne.turn = true;
+            playerTwo.turn = false;
+            newGameboard();
+        }
+
+    }
+
+})
