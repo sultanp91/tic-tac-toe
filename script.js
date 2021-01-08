83c16bb7055e122084c770b82dc1d61dd8815dda
Sultan <sultanp@gmail.com>
2021-01-08 23:11:49 +0000

    updated game logic

--- script.js
+++ script.js
@@ -15,54 +15,65 @@ let newGameboard = function () {
 
 }
 
-let resetGame = function(){
-    boardArray.splice(0, boardArray.length)
-    for(let i = 0; i < 9; i++){
-        boardArray.push("")
-    }
-    playerOne.turn = true;
-    playerTwo.turn = false;
-    return newGameboard()
-}
+
 
 newGameboard();
 
-let playerFactory = function (choice, turn){
+let playerFactory = function (name, choice, turn){
+
+    const gameWon = function () {
+        gameController.gameOver = true;
+        alert(`${this.name} has won`);
+    };
    
     const checkWin = function () {
+
         let playerChoice = this.choice;
         if(boardArray[0] === playerChoice && boardArray[1] === playerChoice && boardArray[2] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[3] === playerChoice && boardArray[4] === playerChoice && boardArray[5] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[6] === playerChoice && boardArray[7] === playerChoice && boardArray[8] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[0] === playerChoice && boardArray[3] === playerChoice && boardArray[6] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[3] === playerChoice && boardArray[4] === playerChoice && boardArray[5] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[1] === playerChoice && boardArray[4] === playerChoice && boardArray[7] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[2] === playerChoice && boardArray[5] === playerChoice && boardArray[8] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[0] === playerChoice && boardArray[4] === playerChoice && boardArray[8] === playerChoice){
-            alert("working")
+            this.gameWon();
         } else if(boardArray[2] === playerChoice && boardArray[4] === playerChoice && boardArray[6] === playerChoice){
-            alert("working")
+            this.gameWon();
         }
-    }
+    };
 
-    return {choice, turn, checkWin};
+    
+   
+
+    return { name, choice, turn, gameWon, checkWin};
 
 }
 
-let playerOne = playerFactory("X", true);
-let playerTwo = playerFactory("O", false)
+let playerOne = playerFactory("Player 1", "X", true);
+let playerTwo = playerFactory("Player 2", "O", false)
 
 let gameController = {
-    playRound: function () {
 
+    gameOver: false,
+
+    resetGame: function(){
+        boardArray.splice(0, boardArray.length);
+        for(let i = 0; i < 9; i++){
+            boardArray.push("");
+        }
+        playerOne.turn = true;
+        playerTwo.turn = false;
+        newGameboard()
     }
+
 }
 
 gameBoardContainer.addEventListener("click", (e) => {
@@ -70,13 +81,13 @@ gameBoardContainer.addEventListener("click", (e) => {
     let arrayIndex = e.target.id;
 
     if(boardArray[arrayIndex] !== "O" && boardArray[arrayIndex] !== "X"){
-        if(playerOne.turn === true){
+        if(playerOne.turn === true && gameController.gameOver === false){
             boardArray[arrayIndex] = playerOne.choice;
             playerOne.turn = false;
             playerTwo.turn = true;
             newGameboard();
             playerOne.checkWin()
-        } else if(playerTwo.turn === true){
+        } else if(playerTwo.turn === true && gameController.gameOver === false){
             boardArray[arrayIndex] = playerTwo.choice;
             playerOne.turn = true;
             playerTwo.turn = false;
