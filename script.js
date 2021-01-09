c64d9bc064b41e997c53707ecd71c8974d1565a2
Sultan <sultanp@gmail.com>
2021-01-09 22:50:21 +0000

    added reset button and ability to add player names to page

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const gameBoardContainer = document.querySelector(".game-board-container");
+const newGameButton = document.querySelector("#newgamebutton");
+const player1button = document.querySelector("#player1button");
+const player2button = document.querySelector("#player2button");
+const player1display = document.querySelector("#player1display");
+const player2display = document.querySelector("#player2display")
+const p1input = document.querySelector("#player1input");
+const p2input = document.querySelector("#player2input");
 
-let boardArray = ["", "", "", "", "", "", "", "", ""]
 
-let newGameboard = function () {
-    gameBoardContainer.textContent = "";
-    for(let i = 0; i < 9; i++){
-       let gridBox = document.createElement("div");
-       gridBox.textContent = boardArray[i];
-       gridBox.classList.add("grid-boxes");
-       gridBox.setAttribute("id", i);
-       gameBoardContainer.append(gridBox);
+const gameBoard = {
 
+    boardArray: ["", "", "", "", "", "", "", "", ""],
+
+    newGameboard: function () {
+        gameBoardContainer.textContent = "";
+        for(let i = 0; i < 9; i++){
+           let gridBox = document.createElement("div");
+           gridBox.textContent = this.boardArray[i];
+           gridBox.classList.add("grid-boxes");
+           gridBox.setAttribute("id", i);
+           gameBoardContainer.append(gridBox);
+    
+        }
+    
     }
 
 }
 
+gameBoard.newGameboard();
 
+let gameController = {
 
-newGameboard();
+    gameOver: false,
+
+    resetGame: function(){
+        gameBoard.boardArray.splice(0, gameBoard.boardArray.length);
+        for(let i = 0; i < 9; i++){
+            gameBoard.boardArray.push("");
+        }
+        playerOne.turn = true;
+        playerTwo.turn = false;
+        gameBoard.newGameboard()
+    }
+
+}
 
 let playerFactory = function (name, choice, turn){
 
@@ -27,7 +53,7 @@ let playerFactory = function (name, choice, turn){
     };
    
     const checkWin = function () {
-
+        let boardArray = gameBoard.boardArray;
         let playerChoice = this.choice;
         if(boardArray[0] === playerChoice && boardArray[1] === playerChoice && boardArray[2] === playerChoice){
             this.gameWon();
@@ -50,9 +76,6 @@ let playerFactory = function (name, choice, turn){
         }
     };
 
-    
-   
-
     return { name, choice, turn, gameWon, checkWin};
 
 }
@@ -60,24 +83,9 @@ let playerFactory = function (name, choice, turn){
 let playerOne = playerFactory("Player 1", "X", true);
 let playerTwo = playerFactory("Player 2", "O", false)
 
-let gameController = {
-
-    gameOver: false,
-
-    resetGame: function(){
-        boardArray.splice(0, boardArray.length);
-        for(let i = 0; i < 9; i++){
-            boardArray.push("");
-        }
-        playerOne.turn = true;
-        playerTwo.turn = false;
-        newGameboard()
-    }
-
-}
 
 gameBoardContainer.addEventListener("click", (e) => {
-    
+    let boardArray = gameBoard.boardArray;
     let arrayIndex = e.target.id;
 
     if(boardArray[arrayIndex] !== "O" && boardArray[arrayIndex] !== "X"){
@@ -85,17 +93,34 @@ gameBoardContainer.addEventListener("click", (e) => {
             boardArray[arrayIndex] = playerOne.choice;
             playerOne.turn = false;
             playerTwo.turn = true;
-            newGameboard();
+            gameBoard.newGameboard();
             playerOne.checkWin()
         } else if(playerTwo.turn === true && gameController.gameOver === false){
             boardArray[arrayIndex] = playerTwo.choice;
             playerOne.turn = true;
             playerTwo.turn = false;
-            newGameboard();
+            gameBoard.newGameboard();
             playerTwo.checkWin()
         }
 
     }
 
+})
+
+newGameButton.addEventListener("click", gameController.resetGame);
+
+player1button.addEventListener("click", function(e){
+    e.preventDefault();
+    playerOne.name = p1input.value;
+    player1display.textContent = `${playerOne.name}`;
+    p1input.value = "";
+})
+
+
+player2button.addEventListener("click", function(e){
+    e.preventDefault();
+    playerTwo.name = p2input.value;
+    player2display.textContent = `${playerTwo.name}`;
+    p2input.value = "";
 })
 
